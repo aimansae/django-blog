ba09b9f6a78bfafe84d80bea9127034045ced345
aimansae <saeedaimann@gmail.com>
2022-10-18 13:59:48 +0000

    Add templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+
+@admin.register(Post)  # almost always like this
+class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'created_on')
+    # You must define search_fields on the related object’s ModelAdmin because the autocomplete search uses it.
+    search_fields = ['title', 'content']
+
+
+@admin.register(Comment)  # almost always like this
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email_address', 'body')
+    action = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,67 @@
-from django.db import models
+# MODELS DOCUMENTATION https://docs.djangoproject.com/en/4.1/topics/db/models/
 
+from django.db import models
+# https://docs.djangoproject.com/en/4.1/topics/auth/default/
+
+from django.contrib.auth.models import User  # to create users
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, 'Published'))
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+# to order the fields base on created on date
+
+    class Meta:
+        ordering = ["-created_on"]
+
+# good practice to put magic method that return a string represent of object
+    def __str__(self):
+        return self.title
+# to reurn total likes of a post
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    # to order the fields base on oldest comment first
+
+    class Meta:
+        ordering = ["created_on"]
+
+# good practice to put magic method that return a string represent of object
+    def __str__(self):
+        return self.title
+# to reurn the comment
+
+    def __str__(self):
+        return f'Comment{self.body} by {self.name}'
+    
+    # NOW MAKE MIGRATION for every change
+    # python3 manage.py makemigrations --dry-run
+    # python3 manage.py makemigrations
+    # python3 manage.py migrate
