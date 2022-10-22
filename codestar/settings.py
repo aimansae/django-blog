258b001f9fc1023663d8b93274858f43b1cba346
aimansae <saeedaimann@gmail.com>
2022-10-22 14:53:06 +0000

    deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -156,8 +156,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
